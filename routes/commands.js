91875d8fce19a7deb695a4cf38b71c74efe8b48d
iGameCreep <gamecreepbs@gmail.com>
2023-07-23 10:11:51 +0200

    added commands route

---
+++ routes/commands.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated } = require('../auth/auth');
+const { getCommands } = require('../funixAPI/commands');
+
+router.get('/commands', ensureAuthenticated, async (req, res) => {
+    const commands = await getCommands();
+    res.render('commands', {
+        user: req.user,
+        commands: commands,
+        monthNames: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]
+    });
+});
+
+module.exports = router;
